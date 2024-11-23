1443c9cee96bc527015ed5c09d09222494002de9
Devansh Rimza <rimzadevansh@gmail.com>
2024-11-23 17:15:35 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,22 @@ const express=require('express');
 
 const app=express();
 
-app.use("/test",(req,res)=> {
-    res.send("Hello from the server");
+
+
+ app.get("/user",(req,res)=> {
+   console.log(req.query);
+    res.send({firstName : "Devansh", lastName : "Rimza"});
  });
 
- app.use("/hello",(req,res)=> {
-    res.send("Hello!");
- });
+ app.post("/user",(req,res)=> {
+   res.send("Data is successfully saved to Database");
+});
 
- app.use("/",(req,res)=> {
-    res.send("Hello from dashboard");
- }); 
+app.delete("/user",(req,res)=> {
+   res.send("Deleted successfully");
+});
+
+ 
  
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...");
