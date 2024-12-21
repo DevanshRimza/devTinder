31e0137b6defc1ffef871c7c93b9fc3978193578
Devansh Rimza <rimzadevansh@gmail.com>
2024-12-21 22:53:16 +0530

    deploying

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dev-tinder-web-git-main-devansh-rimzas-projects.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
